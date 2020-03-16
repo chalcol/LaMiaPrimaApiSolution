63a782418c52f51ac79ff6a32c4099458d79508c
chalcol <chalcol2001@live.com>
2020-03-16 17:37:41 +0100

    Nuovo Controller AuthorCollectionController

---
+++ LaMiaPrimaApi/Controllers/AuthorCollectionsController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Services;
+using LaMiaPrimaApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaMiaPrimaApi.Controllers
+{
+    [ApiController]
+    [Route("api/authorcollections")]
+    public class AuthorCollectionsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibreryRepository;
+        private readonly IMapper _mapper;
+
+        public AuthorCollectionsController(ICourseLibraryRepository courseLibreryRepository, IMapper mapper)
+        {
+            _courseLibreryRepository = courseLibreryRepository ??
+                throw new ArgumentNullException(nameof(courseLibreryRepository));
+            _mapper = mapper ??
+                throw new ArgumentNullException(nameof(courseLibreryRepository));
+            //throw new ArgumentNullException(nameof(courseLibreryRepository));
+        }
+
+        [HttpPost]
+        public ActionResult<IEnumerable<AuthorDto>>CreateAuthorCollection(
+            IEnumerable<AuthorForCreationDto> authorCollection)
+        {
+            var authorEntities = _mapper.Map<IEnumerable<CourseLibrary.API.Entities.Author>>(authorCollection);
+            foreach (var author in authorEntities)
+            {
+                _courseLibreryRepository.AddAuthor(author);
+            }
+            _courseLibreryRepository.Save();
+
+            return Ok();
+           
+        }
+
+       
+    }
+}
