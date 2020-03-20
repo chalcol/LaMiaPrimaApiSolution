f63d5e1ac62f872e35fd7481fda2e0719df3a4a8
chalcol <chalcol2001@live.com>
2020-03-20 13:18:27 +0100

    Custom validation with a custom attribute, Customize Error message, Reporting validation error, Customize validation error response,

--- LaMiaPrimaApi/Models/CourseForCreationDto.cs
+++ LaMiaPrimaApi/Models/CourseForCreationDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LaMiaPrimaApi.ValidationAttributes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,23 +7,24 @@ using System.Threading.Tasks;
 
 namespace LaMiaPrimaApi.Models
 {
-    public class CourseForCreationDto : IValidatableObject
+    [CourseTitleMustBeDifferentFromDescriptionAttribute(ErrorMessage ="Title must be different from description")]
+    public class CourseForCreationDto //: IValidatableObject
     {
-        [Required]
-        [MaxLength(100)]
+        [Required(ErrorMessage ="You should fill out a title.")]
+        [MaxLength(100,ErrorMessage ="The title shouldn't have more than 100 characters")]
         public string Title { get; set; }
 
         
-        [MaxLength(1500)]
+        [MaxLength(500,ErrorMessage = "The description shouldn't have more than 500 characters")]
         public string Description { get; set; }
 
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (Title == Description)
-            {
-                yield return new ValidationResult("The provider description should be different from the title.",
-                    new[] { "CourseForCreationDto" });
-            }
-        }
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Title == Description)
+        //    {
+        //        yield return new ValidationResult("The provider description should be different from the title.",
+        //            new[] { "CourseForCreationDto" });
+        //    }
+        //}
     }
 }
