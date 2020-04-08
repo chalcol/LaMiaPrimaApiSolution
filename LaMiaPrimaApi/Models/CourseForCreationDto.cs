5fc640aa2d87c10575786957061ae57a5573d03e
chalcol <chalcol2001@live.com>
2020-04-08 10:30:24 +0200

    vadation bis

--- LaMiaPrimaApi/Models/CourseForCreationDto.cs
+++ LaMiaPrimaApi/Models/CourseForCreationDto.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using LaMiaPrimaApi.ValidationAttributes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,14 +8,16 @@ using System.Threading.Tasks;
 
 namespace LaMiaPrimaApi.Models
 {
+   // [CourseTitleMustBeDifferentFromDescription (ErrorMessage ="Title must be different from description")]
+    
     public class CourseForCreationDto : IValidatableObject
     {
-        [Required]
-        [MaxLength(100)]
+        [Required(ErrorMessage ="You should fill out a title")]
+        [MaxLength(100, ErrorMessage ="The Title should have more than 100 characters")]
         public string Title { get; set; }
 
         
-        [MaxLength(1500)]
+        [MaxLength(500, ErrorMessage = "The Title should have more than 500 characters")]
         public string Description { get; set; }
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
